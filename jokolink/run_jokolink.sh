bd7635f50bb7941647215ccdcab731c403e53706
Red Artist <redartofcode@gmail.com>
2022-01-16 08:01:02 +0530

    link working on windows 10

--- jokolink/run_jokolink.sh
+++ jokolink/run_jokolink.sh
@@ -9,10 +9,10 @@
 link_name=MumbleLink
 
 # the log lvl of jokolink for file. same options as above
-jl_flog=debug
+jl_log_level=debug
 
 # the filename where jokolink dumps its logs to. must need write permissions to this path
-jl_logfile=./jokolink.log
+jl_logfile_dir=.
 
 # the gap between each copying mumble data to /dev/shm in milliseconds. 
 # 5 ms means it will copy 200 times per second roughly, as long as gw2 is less than 200 fps, we are good :)
@@ -21,7 +21,9 @@ mumble_interval=5
 # the interval to check if gw2 is still running and if so quit. only useful if running outside of lutris, where we will forever be running if not for this check
 # in seconds, each 5 seconds, link will check if gw2 is alive and if it is not, link will quit. usually, lutris will close us forcefully long before
 gw2_check_alive=5
+
+dest_file_path='z:\\dev\\shm\\MumbleLink'
 # 
 # this command will use $WINE from lutris and use the above options to run jokolink in bg
-$WINE "./jokolink.exe" -l $jl_flog -m $link_name -f $jl_logfile -i $mumble_interval -g $gw2_check_alive & 
+$WINE "./jokolink.exe" -l $js_log_level -m $link_name -d $jl_logfile_dir -i $mumble_interval -g $gw2_check_alive -f $dest_file_path & 
 
--- jokolink/src/main.rs
+++ jokolink/src/main.rs
@@ -1,7 +1,16 @@
 use tracing::*;
-use tracing_appender::non_blocking::WorkerGuard;
+
 #[cfg(target_os = "windows")]
-fn main() -> anyhow::Result<()> {
+fn main() {
+    std::panic::set_hook(Box::new(move |info| {
+        tracing::error!("{:#?}", info);
+        
+    }));
+    fake_main().unwrap();
+}
+#[cfg(target_os = "windows")]
+fn fake_main() -> anyhow::Result<()> {
+    use anyhow::{bail, Context};
     use std::io::Write;
     use std::time::Instant;
     // use std::io::BufWriter;
@@ -9,12 +18,63 @@ fn main() -> anyhow::Result<()> {
     use jokolink::win::{create_link_shared_mem, get_xid};
     use std::io::{Seek, SeekFrom};
     // get all the cmd line args and initialize logs.
-    let (mumble_link_key, mumble_refresh_interval, gw2_check_interval, _guard) = create_app();
+    let yml = clap::load_yaml!("app.yml");
+    let m = App::from_yaml(yml).get_matches();
+    let log_level = LevelFilter::from_str(m.value_of("log_level").unwrap_or("debug"))
+        .expect("could not parse log_level option");
+    let logfile_dir = PathBuf::from_str(m.value_of("logfile_dir").unwrap_or("."))
+        .expect("could not parse logfile_dir option");
+    let guard =
+        log_init(log_level, &logfile_dir, Path::new("jokolink.log")).expect("failed to init log");
+        let mumble_key = m.value_of("mumble").unwrap_or("MumbleLink").to_string();
+    let dest_path = PathBuf::from_str(m.value_of("dest_path").unwrap_or("Z:\\dev\\shm\\MumbleLink")).unwrap();
+    let refresh_inverval = Duration::from_millis(
+        u64::from_str(
+            m.value_of("interval")
+                .unwrap_or(&MUMBLE_REFRESH_INTERVAL.to_string()),
+        )
+        .expect("could not parse refresh interval option"),
+    );
+    let gw2_check_interval = Duration::from_secs(
+        u64::from_str(
+            m.value_of("gwcheck")
+                .unwrap_or(&GW2_EXIT_CHECK_INTERVAL.to_string()),
+        )
+        .expect("could not parse gw2 check alive option"),
+    );
 
-    let key: &str = &mumble_link_key;
+    info!("Application Name: {}", env!("CARGO_PKG_NAME"));
+    info!("Application Version: {}", env!("CARGO_PKG_VERSION"));
+    info!("Application Authors: {}", env!("CARGO_PKG_AUTHORS"));
+    info!(
+        "Application Repository Link: {}",
+        env!("CARGO_PKG_REPOSITORY")
+    );
+    info!("Application License: {}", env!("CARGO_PKG_LICENSE"));
+
+    info!("git version details: {}", git_version::git_version!());
+
+    info!(
+        "the file log lvl: {:?}, the logfile directory: {:?}",
+        log_level, &logfile_dir
+    );
+    info!("created app and initialized logging");
+    info!("the mumble link name: {}", &mumble_key);
+    info!(
+        "the mumble refresh interval in milliseconds: {:#?}",
+        refresh_inverval
+    );
+    info!(
+        "the gw2 exit check interval in seconds: {:#?}",
+        gw2_check_interval
+    );
+    info!(
+        "the path to which we write mumble data: {:#?}",
+        dest_path
+    );
 
     // create shared memory using the mumble link key
-    let link = create_link_shared_mem(key);
+    let link = create_link_shared_mem(&mumble_key);
     info!("created shared memory. pointer: {:?}", link);
 
     // check that we created shared memory successfully or panic. get ptr to shared memory
@@ -27,8 +87,7 @@ fn main() -> anyhow::Result<()> {
     })?;
 
     // create a shared memory file in /dev/shm/mumble_link_key_name so that jokolay can mumble stuff from there.
-    let shmpath: PathBuf = ["z:\\", "dev", "shm", key].iter().collect();
-    info!("the path to destination shm file: {:?}", &shmpath);
+    let shmpath: PathBuf = info!("creating the path to destination shm file: {:?}", &dest_path);
 
     let shm = std::fs::File::create(&shmpath);
     info!("shm file created. File: {:?}", &shm);
@@ -55,7 +114,7 @@ fn main() -> anyhow::Result<()> {
         // copy the bytes from mumble link into shared memory file
         CMumbleLink::copy_raw_bytes_into(link_ptr, &mut buffer[..USEFUL_C_MUMBLE_LINK_SIZE]);
         // we sleep for a few milliseconds to avoid reading mumblelink too many times. we will read it around 100 to 200 times per second
-        std::thread::sleep(mumble_refresh_interval);
+        std::thread::sleep(refresh_inverval);
         buffer[(USEFUL_C_MUMBLE_LINK_SIZE + std::mem::size_of::<usize>())..]
             .copy_from_slice(&counter.to_ne_bytes());
         counter += 1;
@@ -81,27 +140,25 @@ fn main() -> anyhow::Result<()> {
                         info!("xid of gw2 window: {:?}", xid);
                     }
                 }
+                if let Some(alive) =
+                    jokolink::win::is_pid_alive(jokolink::win::get_gw2_pid(link_ptr))
+                {
+                    if !alive {
+                        error!("gw2 is not running anymore. exiting...");
+                        return Ok(());
+                    }
+                } else {
+                    bail!("failed to get gw2's alive status");
+                }
             } else {
                 info!("the MumbleLink is not init yet. ");
             }
         }
 
         // write buffer to the file
-        shm.write(&buffer).map_err(|e| {
-            error!(
-                "could not write to shared memory file due to error: {:?}",
-                &e
-            );
-            e
-        })?;
+        shm.write(&buffer).context("could not write to shared memory file due to error")?;
         // seek back so that we will write to file again from start
-        shm.seek(SeekFrom::Start(0)).map_err(|e| {
-            error!(
-                "could not seek to start of shared memory file due to error: {:?}",
-                &e
-            );
-            e
-        })?;
+        shm.seek(SeekFrom::Start(0)).context("could not seek to start of shared memory file due to error")?;
     }
 }
 
@@ -132,58 +189,5 @@ pub fn log_init(
 use clap::App;
 use std::{path::PathBuf, str::FromStr, time::Duration};
 
-pub fn create_app() -> (String, Duration, Duration, WorkerGuard) {
-    let yml = clap::load_yaml!("app.yml");
-    let m = App::from_yaml(yml).get_matches();
-    let log_level = LevelFilter::from_str(m.value_of("log_level").unwrap_or("debug"))
-        .expect("could not parse log_level option");
-    let logfile_dir = PathBuf::from_str(m.value_of("logfile_dir").unwrap_or("."))
-        .expect("could not parse logfile_dir option");
-    let refresh_inverval = Duration::from_millis(
-        u64::from_str(
-            m.value_of("interval")
-                .unwrap_or(&MUMBLE_REFRESH_INTERVAL.to_string()),
-        )
-        .expect("could not parse refresh interval option"),
-    );
-    let gw2_check_interval = Duration::from_secs(
-        u64::from_str(
-            m.value_of("gwcheck")
-                .unwrap_or(&GW2_EXIT_CHECK_INTERVAL.to_string()),
-        )
-        .expect("could not parse gw2 check alive option"),
-    );
-
-    let guard =
-        log_init(log_level, &logfile_dir, Path::new("jokolink.log")).expect("failed to init log");
-    info!("Application Name: {}", env!("CARGO_PKG_NAME"));
-    info!("Application Version: {}", env!("CARGO_PKG_VERSION"));
-    info!("Application Authors: {}", env!("CARGO_PKG_AUTHORS"));
-    info!(
-        "Application Repository Link: {}",
-        env!("CARGO_PKG_REPOSITORY")
-    );
-    info!("Application License: {}", env!("CARGO_PKG_LICENSE"));
-
-    info!("git version details: {}", git_version::git_version!());
-
-    info!(
-        "the file log lvl: {:?}, the logfile directory: {:?}",
-        log_level, &logfile_dir
-    );
-    info!("created app and initialized logging");
-    let key = m.value_of("mumble").unwrap_or("MumbleLink").to_string();
-    info!("the mumble link name: {}", &key);
-    info!(
-        "the mumble refresh interval in milliseconds: {:#?}",
-        refresh_inverval
-    );
-    info!(
-        "the gw2 exit check interval in seconds: {:#?}",
-        gw2_check_interval
-    );
-
-    (key, refresh_inverval, gw2_check_interval, guard)
-}
 const MUMBLE_REFRESH_INTERVAL: u64 = 5;
 const GW2_EXIT_CHECK_INTERVAL: u64 = 5;
